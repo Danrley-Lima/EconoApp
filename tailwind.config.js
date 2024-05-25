3273bf4910208098a9b8ca573cd260947ea55042
Danrley Lima <danrleylac@gmail.com>
2024-05-25 18:51:03 -0300

    feat: adiciona tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        verde: "#31A063",
+        vermelho: "#E64C54",
+        "verde-gradiente-end": "#31A078",
+        "verde-gradiente-start": "#31A05F",
+      },
+      fontFamily: {
+        "DM-Sans": ["DM Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
