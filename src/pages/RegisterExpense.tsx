bed17a55dada633fff69c912bf7e7bf1dd646687
Danrley Lima <danrleylac@gmail.com>
2024-06-11 19:58:58 -0300

    feat: adiciona tamanho ao bottom para não cobrir conteúdo

--- src/pages/RegisterExpense.tsx
+++ src/pages/RegisterExpense.tsx
@@ -1,16 +1,18 @@
 import BottomBar from "../components/BottomBar";
-import Input from "../components/Input";
 import TopBar from "../components/TopBar";
 import FormExpense from "../components/registerExpense/FormExpense";
 
 function RegisterExpense() {
-    return (
-        <main className="w-full">
-            <TopBar title="Cadastrar" />
-            <FormExpense />
-            <BottomBar selected={1} />
-        </main>
-    )
+  return (
+    <main className="w-full">
+      <TopBar title="Cadastrar" />
+      <FormExpense />
+      {/* Gambiarra pra o BottomBar não cobrir o conteúdo */}
+      <div className="h-24">
+        <BottomBar selected={1} />
+      </div>
+    </main>
+  );
 }
 
 export default RegisterExpense;
